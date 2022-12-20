35c5eac7db9f3e3b636e980a44fd3d5779b3afed
DimaShumanski <dshumanski@ecobank.com>
2022-12-20 09:41:24 +0300

    add HotModuleReplacementPlugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -16,6 +16,7 @@ export function buildPlugins({paths, isDev}: BuildOptions): webpack.WebpackPlugi
     }),
     new webpack.DefinePlugin({
       __IS_DEV__: JSON.stringify(isDev),
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin(),
   ]
 }
