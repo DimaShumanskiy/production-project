11e22aff13a1d230770d285923ad0525dee1b99b
DimaShumanskiy <dimaihfn@gmail.com>
2023-01-09 13:02:43 +0300

    add storybook, add decorator storybook and stories

---
+++ config/storybook/preview.js
@@ -0,0 +1,19 @@
+import { addDecorator } from '@storybook/react';
+import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator';
+import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator';
+import { RouteDecorator } from '../../src/shared/config/storybook/RouteDecorator';
+import { Theme } from '../../src/app/providers/ThemeProvider';
+
+export const parameters = {
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/,
+        },
+    },
+};
+
+addDecorator(RouteDecorator);
+addDecorator(StyleDecorator);
+addDecorator(ThemeDecorator(Theme.LIGHT));
